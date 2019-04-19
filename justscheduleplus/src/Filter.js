abb728aa9e5147ed86ce1bedb512cd4af8231000
fifa <teetuch.fifa@mail.kmutt.ac.th>
2019-04-19 18:15:10 +0700

    ส่งค่าจาก state ไป backend

--- justscheduleplus/src/Filter.js
+++ justscheduleplus/src/Filter.js
@@ -17,11 +17,16 @@ class Filter extends Component {
     super(props)
     this.state = {
       showElement: false,
-      period: [],
-      periodName: '',
-      periodOne: '',
-      periodTwo: '',
-      formValues: {}
+      period:[<div>
+        <input type="text" placeholder="Period Name" name='Period Name'  onChange={(e)=> this.state.periodName[this.state.i] = e.target.value} style={{ marginTop: 20, width: 150 }} ></input>
+        <input type="text" placeholder="Period Time" name='Period Time' onChange={(e)=> this.state.periodOne[this.state.i] = e.target.value} style={{ width: 60, marginLeft: 20 }}></input>
+        <span style={{ marginLeft: 10 }}> - </span>
+        <input type="text" placeholder="Period Time" name='Period Time2'  onChange={(e)=> this.state.periodTwo[this.state.i] = e.target.value} style={{ width: 60, marginLeft: 10 }}></input>
+      </div>],
+      periodName: [],
+      periodOne: [],
+      periodTwo: [],
+      i: 0
     }
   }
   onClose = (e) => {
@@ -33,20 +38,22 @@ class Filter extends Component {
 
 
   addPeriod = () => {
+    this.setState({i: this.state.i+1 })
     var periods = (
       <div>
-        <input type="text" placeholder="Period Name" name='Period Name' value={this.state.formValues["Period Name"]} onChange={this.handleChange.bind(this)} style={{ marginTop: 20, width: 150 }} ></input>
-        <input type="text" placeholder="Period Time" name='Period Time' value={this.state.formValues["Period Time"]} onChange={this.handleChange.bind(this)} style={{ width: 60, marginLeft: 20 }}></input>
+        <input type="text" placeholder="Period Name" name='Period Name'  onChange={(e)=> this.state.periodName[this.state.i] = e.target.value} style={{ marginTop: 20, width: 150 }} ></input>
+        <input type="text" placeholder="Period Time" name='Period Time' onChange={(e)=> this.state.periodOne[this.state.i] = e.target.value} style={{ width: 60, marginLeft: 20 }}></input>
         <span style={{ marginLeft: 10 }}> - </span>
-        <input type="text" placeholder="Period Time" name='Period Time2' value={this.state.formValues["Period Time2"]} onChange={this.handleChange.bind(this)} style={{ width: 60, marginLeft: 10 }}></input>
+        <input type="text" placeholder="Period Time" name='Period Time2'  onChange={(e)=> this.state.periodTwo[this.state.i] = e.target.value} style={{ width: 60, marginLeft: 10 }}></input>
       </div>
     )
-    return this.setState({ period: this.state.period.concat(periods) })
+   console.log(this.state.periodName)
+    return this.setState({ period: this.state.period.concat(periods)})
   }
 
   disablePeriod = () => {
     var lenght = this.state.period.length
-    if (lenght == 5) {
+    if (lenght == 4) {
       return true
     } else {
       return false
@@ -63,36 +70,32 @@ class Filter extends Component {
 
   handleSubmit = (event) => {
     event.preventDefault()
-    console.log(this.state.formValues);
   }
 
-  handleChange = (event) => {
+  handleChange = (event,i) => {
     event.preventDefault();
-    let formValues = this.state.formValues;
-    let name = event.target.name;
-    let value = event.target.value;
-
-    formValues[name] = value;
-
-    this.setState({ formValues })
   }
 
-  // onAfterInsertRow = (addPeriod) => {
-  //   console.log(addPeriod)
-  //   const Url='http://localhost:8080/period';
+  onAfterInsertRow = () => {
+    console.log(this.state.periodName)
+    const Url='http://localhost:8080/period';
 
-  //   const othepram={
-  //     headers:{
-  //       "content-type":"application/json; charset=UTF-8"
-  //     },
-  //     body: JSON.stringify(addPeriod),
-  //     method:"POST"
-  //   };
-  //   fetch(Url,othepram)
-  //   .then(data=>{return data.json()})
-  //   .then(res=>{console.log(res)})
-  //   .catch(error=>console.log(error))
-  // }
+    const othepram={
+      headers:{
+        "content-type":"application/json; charset=UTF-8"
+      },
+      body: JSON.stringify({
+        periodName: this.state.periodName,
+        periodOne: this.state.periodOne,
+        periodTwo: this.state.periodTwo 
+      }),
+      method:"POST"
+    };
+    fetch(Url,othepram)
+    .then(data=>{return data.json()})
+    .then(res=>{console.log(res)})
+    .catch(error=>console.log(error))
+  }
 
 
   render() {
@@ -115,21 +118,14 @@ class Filter extends Component {
             <Row>
               <Col md={6} className="Period">
                 <button className="Addbutton" onClick={this.addPeriod} disabled={this.disablePeriod()}>Add+</button>
-                <form method='POST' onSubmit={this.handleSubmit.bind(this)}>
-                  <div>
-                    <input type="text" placeholder="Period Name" name='Period Name' value={this.state.formValues["Period Name"]} onChange={this.handleChange.bind(this)} style={{ marginTop: 20, width: 150 }} ></input>
-                    <input type="text" placeholder="Period Time" name='Period Time' value={this.state.formValues["Period Time"]} onChange={this.handleChange.bind(this)} style={{ width: 60, marginLeft: 20 }}></input>
-                    <span style={{ marginLeft: 10 }}> - </span>
-                    <input type="text" placeholder="Period Time" name='Period Time2' value={this.state.formValues["Period Time2"]} onChange={this.handleChange.bind(this)} style={{ width: 60, marginLeft: 10 }}></input>
-                    <img src={error} style={{ width: 15, height: 15, marginLeft: 15, marginTop: 30 }} onClick={() => this.remove()}></img>
-                  </div>
-                  {/* {this.state.period.map((event, key) => {
+                
+                   {this.state.period.map((event, key) => {
                     return <div style={{ display: "flex" }}>{event}
                       <img src={error} style={{ width: 15, height: 15, marginLeft: 15, marginTop: 30 }} onClick={() => this.remove(key)}></img>
                     </div>
-                  })} */}
-                  <input className="btn btn-primary" type="submit" value="Submit" />
-                </form>
+                  })} 
+                  <input style={{ marginLeft: 280, position:"fixed", top:580}}className="btn btn-primary" onClick={()=>this.onAfterInsertRow()} type="submit" value="Submit" />
+                
               </Col>
               <Col md={6} style={{ backgroundColor: "#07889B", height: 372 }}>
                 <p style={{ marginTop: 70, color: "white" }} className="Advice">JS+</p>
