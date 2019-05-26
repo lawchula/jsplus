711a80c1bc63f28ef932f0bb0ff80e0151d36966
TaloeyToey <toey_and_guy@hotmail.com>
2019-05-26 20:10:24 +0700

    gitignor build

---
+++ justscheduleplus/build/precache-manifest.ebaa9c6778d4746ef2e28266bededc85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "878ae1e0ec9a86cf328f",
+    "url": "/static/js/main.cccef8c4.chunk.js"
+  },
+  {
+    "revision": "afe60f4aafdb7ba58485",
+    "url": "/static/js/2.9cb9fbbb.chunk.js"
+  },
+  {
+    "revision": "878ae1e0ec9a86cf328f",
+    "url": "/static/css/main.392ee59d.chunk.css"
+  },
+  {
+    "revision": "afe60f4aafdb7ba58485",
+    "url": "/static/css/2.b3780bc0.chunk.css"
+  },
+  {
+    "revision": "db5ae0b9a2a5b64f4216312f1e546db9",
+    "url": "/index.html"
+  }
+];
