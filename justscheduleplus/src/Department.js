d111dc89b63d5c385b49a316d0b7226caba5aa0c
fifa <teetuch.fifa@mail.kmutt.ac.th>
2019-10-22 17:48:31 +0700

    Departments

--- justscheduleplus/src/Department.js
+++ justscheduleplus/src/Department.js
@@ -4,6 +4,7 @@ import * as firebase from 'firebase';
 import plus from './Images/plus.png';
 import CreateDepartment from './CreateDepartment';
 import Header from './Header';
+import url from './url';
 
 class Department extends Component {
 
@@ -11,7 +12,8 @@ class Department extends Component {
         super(props)
         this.state = {
             company:[],
-            showdepartment: false
+            showdepartment: false,
+            department:[{name:"test",telno:"029131944",member:"10"}]
         }
     }
 
@@ -49,11 +51,23 @@ class Department extends Component {
 
 
     render(){
-        const {company} = this.state
+        const {company,department} = this.state
         const compayname = company.map((event,i) =>  {return <span className="second-description" key={i}>{ event.Company_Name}</span>})
         const companymail = company.map((event,i) =>  {return <span className="second-description" key={i}>{event.Company_Mail}</span>})
         const companytel = company.map((event,i) =>  {return <span className="second-description" key={i}>{event.Company_Tel}</span>})
-        const companypicture = company.map((event,i) =>  {return <img key={i} className="company-pictures" src={event.Company_Picture} ></img>}) 
+        const companypicture = company.map((event,i) =>  {return <img key={i} className="company-pictures" src={event.Company_Picture} ></img>})
+        const departments = department.map((event,i) => {return <div className="departments">
+             <div className="dp-img1">
+
+            </div>
+            <div className="dp-sh">
+              <span>Department : {event.name}</span>
+              <span>Telno : {event.telno} </span>
+              <span>Member : {event.member}</span>
+              <button className="b-mn">Manage</button>
+            </div>
+
+        </div>} )
        
 
 
@@ -74,11 +88,14 @@ class Department extends Component {
                         <div className="create-department" onClick={this.showCreateDepartment}> 
                            <img src={plus} style={{height:32}}></img>
                         </div>
-                        <span style={{marginLeft:5}}>Create Department</span>
-                    </div>
+                        <span style={{marginLeft:10}}>create department</span>
+                        </div>
+                        {departments}
+                        
                 </div>
                 <CreateDepartment show={this.state.showdepartment} onClose={this.showCreateDepartment}></CreateDepartment>
             </div>
+
         );
     }
 
