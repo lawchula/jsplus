13419e87890b9a4d5e5c3436e0ad3b89d2227639
ConflexZ <wutkungza9@hotmail.com>
2019-11-12 20:53:54 +0700

    Commit

--- justscheduleplus/src/App.js
+++ justscheduleplus/src/App.js
@@ -1,45 +1,47 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route} from "react-router-dom"
-import './Css/App.css';
-import Schedule from './Schedule';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./Css/App.css";
+import Schedule from "./Schedule";
 // import Home from './OldHome';
-import User from './User';
-import EditProfile from './EditProfile'
-import TestComponent2 from './TestComponent2';
-import Home from './Home';
+import User from "./User";
+import EditProfile from "./EditProfile";
+import TestComponent2 from "./TestComponent2";
+import Home from "./Home";
 // import Request from './RequestPopup';
-import CreateCompany from './CreateCompany';
-import Company from './Company';
-import Department from './Department';
-
-
-
-
+import CreateCompany from "./CreateCompany";
+import Company from "./Company";
+import Department from "./Department";
+import AdminHowToUse from "./AdminHowToUse";
+import StaffHowToUse from "./StaffHowToUse";
+import ManagerHowToUse from "./ManagerHowToUse";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Router>
-          <Route exact path="/" component = {Home} />
-          <Route path = "/Schedule" component = {Schedule} />
-          <Route path = "/User" component = {User} />
-          <Route path = "/EditProfile" component = {EditProfile} />
-          <Route path = "/Company" component ={Company} />
-          <Route path = "/Department" component = {Department} />
-          <Route path = "/Test" component ={TestComponent2} />
+          <Route exact path="/" component={Home} />
+          <Route path="/Schedule" component={Schedule} />
+          <Route path="/User" component={User} />
+          <Route path="/EditProfile" component={EditProfile} />
+          <Route path="/Company" component={Company} />
+          <Route path="/Department" component={Department} />
+          <Route path="/Test" component={TestComponent2} />
+          <Route path="/AdminHowToUse" component={AdminHowToUse} />
+          <Route path="/StaffHowToUse" component={StaffHowToUse} />
+          <Route path="/ManagerHowToUse" component={ManagerHowToUse} />
         </Router>
-      {/* <Schedule/> 
+        {/* <Schedule/> 
       {/* <User></User> */}
-       {/* <TestComponent2/> */}
-       {/* <Register/> */}
-       {/* <Home></Home> */}
-       {/* <Department></Department> */}
-       {/* <CreateCompany></CreateCompany>  */}
-         {/* <CreateDepartment></CreateDepartment>
+        {/* <TestComponent2/> */}
+        {/* <Register/> */}
+        {/* <Home></Home> */}
+        {/* <Department></Department> */}
+        {/* <CreateCompany></CreateCompany>  */}
+        {/* <CreateDepartment></CreateDepartment>
        {/* <NewHomePage></NewHomePage> */}
-       {/* <DepartmentDP></DepartmentDP> */}
-       {/* <TestComponent2></TestComponent2> */}
+        {/* <DepartmentDP></DepartmentDP> */}
+        {/* <TestComponent2></TestComponent2> */}
       </div>
     );
   }
